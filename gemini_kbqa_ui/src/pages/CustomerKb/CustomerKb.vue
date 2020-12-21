cd2037fd647ddc8152945acc7fcad2754c68f897
zhangyan1307 <1014638540@qq.com>
2020-12-21 16:36:31 +0800

    第一版

--- gemini_kbqa_ui/src/pages/CustomerKb/CustomerKb.vue
+++ gemini_kbqa_ui/src/pages/CustomerKb/CustomerKb.vue
@@ -2,7 +2,7 @@
 <div class="CustomerKb">
   <el-container style="height: 700px; border: 1px solid #eee">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu :default-openeds="['1', '3']"@select="select">
+      <el-menu :default-openeds="['1', '3']" @select="select">
         <el-submenu index="1" style="background: rgb(24, 143, 255)">
           <template slot="title"
             ><i class="el-icon-message"></i>全部分类</template
