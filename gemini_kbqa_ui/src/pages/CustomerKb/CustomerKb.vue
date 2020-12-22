5fa3f1a1a2b43b1940dc8bf725c62d78e622e02a
xiangxy26878 <xiangxy26878@hundsun.com>
2020-12-22 23:39:52 +0800

    提交变更

--- gemini_kbqa_ui/src/pages/CustomerKb/CustomerKb.vue
+++ gemini_kbqa_ui/src/pages/CustomerKb/CustomerKb.vue
@@ -1,21 +1,13 @@
 <template>
   <el-container style="height: 700px; border: 1px solid #eee">
   <el-aside width="200px"  style="background-color: rgb(238, 241, 246)">
-    <el-menu :default-openeds="['1', '3']">
+    <el-menu :default-openeds="['1']">
       <el-submenu index="1">
         <template slot="title"><i class="el-icon-message"></i>全部分类</template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
           <el-menu-item index="1-1">选项1</el-menu-item>
           <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
           <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项4-1</el-menu-item>
-        </el-submenu>
+          <el-menu-item index="1-5">选项4</el-menu-item>
       </el-submenu>
       <el-menu-item index="2">管理分类</el-menu-item>
     </el-menu>
@@ -23,24 +15,23 @@
   
   <el-container>
     <el-header style="text-align: right; font-size: 12px">
-      <el-dropdown>
-        <i class="el-icon-setting" style="margin-right: 15px"></i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>查看</el-dropdown-item>
-          <el-dropdown-item>新增</el-dropdown-item>
-          <el-dropdown-item>删除</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
+      
       <span>王小虎</span>
     </el-header>
     
     <el-main>
       <el-table :data="tableData">
-        <el-table-column prop="date" label="日期" width="140">
+        <el-table-column prop="date" label="资料名称" width="140">
         </el-table-column>
-        <el-table-column prop="name" label="姓名" width="120">
+        <el-table-column prop="name" label="内容摘要" width="120">
         </el-table-column>
-        <el-table-column prop="address" label="地址">
+         <el-table-column prop="name" label="类型" width="120">
+        </el-table-column>
+        <el-table-column prop="address" label="创建人">
+        </el-table-column>
+        <el-table-column prop="date" label="创建日期">
+        </el-table-column>
+        <el-table-column prop="date" label="操作">
         </el-table-column>
       </el-table>
     </el-main>
@@ -69,10 +60,12 @@
         address: '上海市普陀区金沙江路 1518 弄'
       };
       return {
-        tableData: Array(20).fill(item)
+        tableData: Array(20).fill(item),
+        menuDataList: null
       }
     },
     mounted: function(){
+      
 
     },
     methods: {
